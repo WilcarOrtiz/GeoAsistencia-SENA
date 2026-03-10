797d5906dd10955588e23b0c56872f0ff75f6dae
Wilcar <ortizcolpaswilcardaniel@gmail.com>
2026-03-09 20:58:22 -0500

    cambio de pc a MacBook

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -36,9 +36,7 @@ Nivel 3 (dependen del nivel 2):
 
 
 Para LECTURA (findOne, findAll, getUserProfile): Sigue usando QueryBuilder. Es lo más eficiente. En bases de datos relacionales, el usuario es el eje central y es normal que "conozca" sus extensiones (Estudiante/Profesor) para mostrar la información rápido.
-
-Tip: Solo selecciona las columnas que realmente necesitas con .select(), no traigas toda la entidad si no la usas.
-
+Tip: Solo selecciona las columnas que realmente necesitas con .select(), no traigas toda la entidad si no la usas
 Para ESCRITURA (createUser, updateRoles): Usa Servicios o Eventos. Aquí el rendimiento de milisegundos no es tan crítico como la integridad de los datos. Si un createUser tarda 200ms en lugar de 100ms por llamar a otro servicio, no importa, siempre que la lógica sea sólida.
   
   */
